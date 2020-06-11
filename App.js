e1e24b75eabea550459023cc9cc7c2a1e68c6de1
Zaraco <zahrateymouri90@gmail.com>
2020-06-11 13:17:23 +0200

    login added

--- App.js
+++ App.js
@@ -1,27 +1,41 @@
 import React from 'react';
-import {Container, Header, Content, Form, Item, Input, Label} from 'native-base';
-import {View, Text} from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Text,
+} from 'native-base';
+import {Alert, Button} from 'react-native';
 
 class App extends React.Component {
-    render() {
-        return (
-            <Container>
-                <Header/>
-                <Content>
-                    <Form>
-                        <Item floatingLabel>
-                            <Label>Username</Label>
-                            <Input/>
-                        </Item>
-                        <Item floatingLabel last>
-                            <Label>Password</Label>
-                            <Input/>
-                        </Item>
-                    </Form>
-                </Content>
-            </Container>
-        );
-    }
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <Form>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+            <Button
+              title="Press me"
+              color="#f194ff"
+              onPress={() => Alert.alert('Button with adjusted color pressed')}
+            />
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
 }
 
 export default App;
